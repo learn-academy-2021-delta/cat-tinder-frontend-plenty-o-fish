49eb6296fddbcd488197e52f39c721739fbfba29
Robert Cook <90288134+bobobnj@users.noreply.github.com>
2021-10-28 11:23:41 -0700

    Finished with front-end

---
+++ src/pages/FishShow.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class FishNew extends Component {
+    render() {
+        return (
+            <div>
+                <h3>I am the Show</h3>
+            </div>
+        )
+    }
+}
+
