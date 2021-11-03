c500392f74146c53fe29300342f8ad0167a8cfbd
brandonho2000 <bradho099@gmail.com>
2021-11-03 14:47:09 -0500

    frontend fetch

--- src/App.js
+++ src/App.js
@@ -11,18 +11,64 @@ import NotFound from "./pages/NotFound";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
-import fish from "./MockFish.js";
+// import fish from "./MockFish.js";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      fish: fish,
+      fish: [],
     };
   }
-  createFish = (newFish) => {
-    console.log(newFish);
+
+  componentDidMount() {
+    this.readFish();
+  }
+  readFish = () => {
+    fetch("http://localhost:3000/fish")
+      .then((response) => response.json())
+      .then((fishArray) => this.setState({ fish: fishArray }))
+      .catch((errors) => console.log(errors));
   };
+
+  createFish = (newFish) => {
+    fetch("http://localhost:3000/fish", {
+      body: JSON.stringify(newFish),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    })
+      .then((response) => response.json())
+      .then((payload) => this.readCat())
+      .catch((errors) => console.log(errors));
+  };
+
+  updateFish = (editedFish, id) => {
+    fetch(`http://localhost:3000/fish/${id}`, {
+      body: JSON.stringify(editedFish),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "PATCH",
+    })
+      .then((response) => response.json())
+      .then((payload) => this.readFish())
+      .catch((errors) => console.log(errors));
+  };
+
+  deleteFish = (id) => {
+    fetch(`http://localhost:3000/fish/${id}`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then((payload) => this.readFish())
+      .catch((errors) => console.log("delete errors:", errors));
+  };
+
   render() {
     return (
       <Router>
@@ -39,14 +85,22 @@ class App extends Component {
             render={(props) => {
               let id = props.match.params.id;
               let fish = this.state.fish.find((f) => f.id === +id);
-              return <FishShow fish={fish} />;
+              return <FishShow fish={fish} deleteCat={this.deleteCat}/>;
             }}
           />
           <Route path="/fishshow" component={FishShow} />
-          <Route 
-          path="/fishnew" 
-          render={(props) => <FishNew createFish={this.createFish} />}
+          <Route
+            path="/fishnew"
+            render={(props) => <FishNew createFish={this.createFish} />}
           />
+            <Route
+              path="/fishedit/:id"
+              render={(props) => {
+                let id = props.match.params.id
+                let fish = this.state.fish.find(c => c.id === +id)
+                return <FishEdit fish={fish} updateFish={this.updateFish } id={id} />
+              }}
+            />
           <Route path="/fishedit" component={FishEdit} />
           <Route component={NotFound} />
         </Switch>
--- src/pages/FishEdit.js
+++ src/pages/FishEdit.js
@@ -1,11 +1,79 @@
 import React, { Component } from 'react'
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap'
+import { Redirect } from 'react-router-dom'
 
 export default class FishEdit extends Component {
-    render() {
+    constructor(props){
+        super(props)
+        this.state = {
+          form: {
+            name: this.props.fish ? this.props.fish.name : "",
+            age: this.props.fish ? this.props.fish.age : "",
+            enjoys: this.props.fish ? this.props.fish.enjoys : "",
+          },
+          submitted: false
+        }
+      }
+    
+      handleChange = (e) => {
+        let { form } = this.state
+        form[e.target.name] = e.target.value
+        this.setState({form: form})
+      }
+      handleSubmit = () => {
+        this.props.updateFish(this.state.form, this.props.id)
+        this.setState({submitted: true})
+      }
+      ender() {
+
         return (
-            <div>
-                <h3>I am the Edit</h3>
-            </div>
+          <div className="page-body">
+            <h3>Update Fish Information</h3>
+            <Form>
+              <FormGroup>
+                <Label for="name">
+                  Fishes Name
+                </Label>
+                <Input
+                  name="name"
+                  type="text"
+                  onChange={this.handleChange}
+                  value={this.state.form.name}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="age">
+                  Fishes Age
+                </Label>
+                <Input
+                  name="age"
+                  type="number"
+                  min="0"
+                  max="20"
+                  onChange={this.handleChange}
+                  value={this.state.form.age}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="enjoys">
+                  Fishes Hobbies and Enjoyment
+                </Label>
+                <Input
+                  name="enjoys"
+                  type="text"
+                  onChange={this.handleChange}
+                  value={this.state.form.enjoys}
+                />
+              </FormGroup>
+              <Button
+                onClick={this.handleSubmit}
+                name="submit"
+              >
+                Update Fish
+              </Button>
+            </Form>
+            {this.state.submitted && <Redirect to={`/fishshow/${this.props.fish.id}`} />}
+          </div>
         )
+      }
     }
-}
